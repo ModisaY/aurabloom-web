989f49ba63f9b619780699e24340f3c7b3729738
Modisa Nallaperuma <modisa.20230363@iit.ac.lk>
2025-02-23 11:37:46 +0530

    changes

--- project/src/components/Navigation.tsx
+++ project/src/components/Navigation.tsx
@@ -12,7 +12,7 @@ const Navigation = () => {
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
           <Link to="/" className="flex items-center space-x-2">
-            <img src="src/img/logo.png" alt="AuraBloom Logo" className="h-8 w-8" />
+            <img src="/img/logo.png" alt="AuraBloom Logo" className="h-8 w-8" />
             <span className="text-2xl font-bold bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent">
               AuraBloom
             </span>
